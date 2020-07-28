d27b112009bec1fede4ad4ed7da8a0a04def288f
Dimagerasimenko <holy_priest@mail.ru>
2020-07-28 22:51:07 +0300

    lesson7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,23 +4,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <HelloWorld />
+        <IamgoinfToNewWorld />
     </div>
+
   );
+}
+
+function IamgoinfToNewWorld() {
+    return (
+        <div>
+          <a href="#k"> IamgoinfToNewWorld</a>
+        </div>
+
+    );
+
+}
+
+function HelloWorld() {
+    return (
+        <div>
+        <a href="#s">hello world</a>
+        </div>
+    );
 }
 
 export default App;
