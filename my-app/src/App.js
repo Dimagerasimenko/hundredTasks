bb2c9a1a360db7c3b969947795ffbb94be787bf1
Dimagerasimenko <holy_priest@mail.ru>
2020-07-29 23:12:20 +0300

    lesson_11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import './App.css';
 
-import IamgoinfToNewWorld from './Iamgoingtonewworld'
-import HelloWorld from "./helloWorld";
+
 
 function App() {
   return (
-      <div>
-        <HelloWorld />
-        <IamgoinfToNewWorld />
+      <div className="app-wrapper">
+        <header className="header">
+          <img src='https://lendmeyourear.net/images/how-to-embed-silverlight-using-javascript/cover.jpg' alt="img"></img>
+        </header >
+        <nav className="nav">
+          <div>
+            <a href="#s">Profile</a>
+          </div>
+          <div>
+            <a href="#s">Messages</a>
+          </div>
+          <div>
+            <a href="#s">News</a>
+          </div>
+          <div>
+            <a href="#s">Music</a>
+          </div>
+        </nav>
+        <div className="content">
+          <div>
+            <img src='https://lh3.googleusercontent.com/proxy/4mZ91qDHH7ZUoGIk45_o1sg25Au5WQVpHSO3bpV9mk9f-jHEQ1B7Ec2uMKPUiL_QTqTrJN2xqujZBjLwjqwuSAQ0saYicZopQueswrcPIROOib-MAGTkg_Degu0v_M0Dlj0O6T83eiY'
+        alt='img'></img>
+          </div>
+            <div>
+              ava+description
+            </div>
+            <div>
+              My post
+              <div>
+                New post
+              </div>
+              <div>
+                 <div>
+                     Post 1
+                 </div>
+                 <div>
+                     Post 2
+                 </div>
+
+              </div>
+            </div>
+
+        </div>
     </div>
 
   );
