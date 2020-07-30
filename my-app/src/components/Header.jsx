a5e51d07959aab65a8c561b14da1dc5d427d8234
Dimagerasimenko <holy_priest@mail.ru>
2020-07-30 11:58:49 +0300

    'lesson_12'

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className="header">
+            <img src='https://lendmeyourear.net/images/how-to-embed-silverlight-using-javascript/cover.jpg' alt="img"></img>
+        </header >
+    );
+}
+
+export default Header;
---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div className="content">
+            <div>
+                <img src='https://lh3.googleusercontent.com/proxy/4mZ91qDHH7ZUoGIk45_o1sg25Au5WQVpHSO3bpV9mk9f-jHEQ1B7Ec2uMKPUiL_QTqTrJN2xqujZBjLwjqwuSAQ0saYicZopQueswrcPIROOib-MAGTkg_Degu0v_M0Dlj0O6T83eiY'
+                     alt='img'></img>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
